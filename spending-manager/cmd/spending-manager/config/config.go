3127b27462d6e40d98ea731146766299e3f52631
IshenkoAI <oleks1o@yandex.ru>
2023-12-13 12:52:39 +0300

    [ST-B-003] Создать модуль spending-manager

---
+++ spending-manager/cmd/spending-manager/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var Config appConfig
+
+type appConfig struct {
+	// Пример переменной, загружаемой в функции LoadConfig
+	ConfigVar string
+}
+
+// LoadConfig загружает конфигурацию из файлов
+func LoadConfig(configPaths ...string) error {
+	v := viper.New()
+	v.SetConfigName("application") // <- имя конфигурационного файла
+	v.SetConfigType("yml")
+	v.AutomaticEnv()
+	for _, path := range configPaths {
+		v.AddConfigPath(path) // <- // путь для поиска конфигурационного файла в
+	}
+	if err := v.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read the configuration file: %s", err)
+	}
+	return v.Unmarshal(&Config)
+}
