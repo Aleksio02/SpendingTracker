c53206d5248e4f96ae0fad49b94943373b4156c1
LisenkoIA <vanyalisenko1985@gmail.com>
2024-01-08 19:15:57 +0300

    Создал модуль работы с пунктами трат

---
+++ spending-manager/cmd/spending-manager/model/response/system_test_response.go
@@ -0,0 +1,6 @@
+package response
+
+type SystemTestResponse struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
---
+++ spending-manager/go.mod
@@ -0,0 +1,9 @@
+module spending-manager
+
+go 1.21
+
+require (
+github.com/spf13/viper v1.4.0
+github.com/gin-gonic/gin v1.4.0
+
+)
